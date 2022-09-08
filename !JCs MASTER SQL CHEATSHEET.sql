cca15ef6392e21f62b7fda9aa57da17ddabc77f0
Jonathan Cachat, PhD <github@jcachat.com>
2022-09-08 09:13:08 -0400

    Create !JCs MASTER SQL CHEATSHEET.sql
    first .sql file

---
+++ !JCs MASTER SQL CHEATSHEET.sql
@@ -0,0 +1,25 @@
+Custom List / Cheat Sheet of SQL Functionality to Learn
+
+=====Filtering=======
+ORDER BY col ASC or DESC
+
+SELECT DISTINCT
+
+
+
+=====Aggregations & GROUP BY / ORDER BY=================
+WHERE col >= 3
+WHERE col BETWEEN # AND #
+WHERE col = 'string'
+WHERE col IN ('string', 'string') --contain string values
+WHERE col1 LIKE 'j%' AND/OR col2 NOT LIKE '%z'; --records that start with j and end with z
+WHERE col >= 3
+WHERE col BETWEEN # AND #
+WHERE col = 'string'
+WHERE col IN ('string', 'string') --contain string values
+WHERE col1 LIKE 'j%' AND/OR col2 NOT LIKE '%z'; --records that start with j and end with z
+
+
+
+=====JOINS=======
+SC or DESC
